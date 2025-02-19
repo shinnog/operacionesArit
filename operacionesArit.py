41878544a030c95a197748b10f2e4e7f0f83b2e2
JDiego-13 <77897287+JDiego-13@users.noreply.github.com>
2025-02-18 18:22:40 -0600

    Add files via upload

---
+++ operacionesArit.py
@@ -0,0 +1,16 @@
+
+def suma(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+def resta(num1, num2):
+    return num1 - num2
+
+def dividir(dividendo, divisor):
+    return dividendo / divisor
+
+def cuadrado(numero):
+    return numero * numero
+
+def multiplicar(num1, num2):
+    return num1 * num2
